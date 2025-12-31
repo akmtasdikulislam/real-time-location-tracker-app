3a63231dbf4a9160af5027728a6d429c6a3cf318
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-12-31 14:20:44 +0600

    Initialized client part

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center my-10">
+        Welcome to Real Time Location Tracker App
+      </h1>
+    </div>
+  );
+};
+
+export default App;
